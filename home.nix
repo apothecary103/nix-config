969a64e5f27640e0b387f1d5719e9f51eff0aec3
apothecary <113787039+yukariha@users.noreply.github.com>
2026-03-14 23:07:12 +0200

    Initial commit

---
+++ home.nix
@@ -0,0 +1,166 @@
+{ pkgs, username, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should manage.
+  home.username = username;
+  home.homeDirectory = "/Users/${username}";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  home.stateVersion = "25.11"; 
+
+  # 3. Install Apps/Packages
+  home.packages = with pkgs; [
+    # CLI Tools
+    ripgrep
+    fd
+    yazi
+    eza
+    aria2
+    fastfetch
+    chafa
+    zellij
+    wget
+    
+    # GUI Applications 
+    prismlauncher
+    signal-desktop
+    vesktop
+    mpv
+    cinny-desktop
+    
+    # Fonts (See next section for more font config)
+    maple-mono.NF-CN
+    aporetic
+  ];
+
+  # 4. Configure Fonts
+  # On macOS, this tells Home Manager to link fonts to ~/Library/Fonts
+  fonts.fontconfig.enable = true;
+
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name  = "apothecary";
+        email = "113787039+yukariha@users.noreply.github.com";
+      };
+      init.defaultBranch = "main";
+    };
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    initContent = ''
+      setopt PROMPT_SUBST
+
+      function _nushell_left_prompt() {
+          local p="''${(%):-%~}"
+          local path_color="%B%F{2}"
+          local sep_color="%B%F{10}"
+        
+          if [[ $EUID -eq 0 ]]; then
+              path_color="%B%F{1}"
+              sep_color="%B%F{9}"
+          fi
+
+          local colored_path="''${p//\//$sep_color/$path_color}"
+          echo "''${path_color}''${colored_path}%b%f"
+      }
+
+      function _nushell_right_prompt() {
+          local last_status=$?
+          local magenta="%F{5}"
+          local green="%F{2}"
+          local magenta_ul="%U%F{5}"
+          local reset="%u%f"
+          local date_str="$(date +'%m/%d/%Y %I:%M:%S %p')"
+        
+          local time_colored="''${date_str//\//$green/$magenta}"
+          time_colored="''${time_colored//:/$green:$magenta}"
+          time_colored="''${time_colored//AM/''${magenta_ul}AM}"
+          time_colored="''${time_colored//PM/''${magenta_ul}PM}"
+          time_colored="''${magenta}''${time_colored}''${reset}"
+        
+          local exit_code_str=""
+          if [[ $last_status -ne 0 ]]; then
+              exit_code_str="%B%F{1}''${last_status}%b%f "
+          fi
+        
+          echo "''${exit_code_str}''${time_colored}"
+      }
+
+      PROMPT='$(_nushell_left_prompt)%B%F{14}> %b%f'
+      PROMPT2='::: '
+      RPROMPT='$(_nushell_right_prompt)'
+    '';
+  };
+
+  programs.helix = {
+    enable = true;
+    defaultEditor = true;
+    
+    themes = {
+      catppuccin_macchiato_transparent = {
+        inherits = "catppuccin_macchiato";
+        "ui.background" = { bg = "none"; };
+      };
+    };
+
+    settings = {
+      theme = "catppuccin_macchiato_transparent";
+
+      editor = {
+        true-color = true;
+        line-number = "relative";
+        mouse = false;
+        cursorline = true;
+        bufferline = "multiple";
+        default-line-ending = "lf";
+        lsp.display-messages = true;
+        
+        cursor-shape = {
+          insert = "bar";
+          select = "underline";
+        };
+        
+        file-picker = {
+          hidden = false;
+          git-ignore = true;
+        };
+
+        soft-wrap = {
+          enable = true;
+        };
+      };
+    };
+  };
+
+  programs.ghostty = {
+    enable = true;
+    package = pkgs.ghostty-bin;
+    settings = {
+      theme = "Catppuccin Macchiato";
+      font-family = "Maple Mono NF CN";
+      font-size = 18;
+
+      # macOS specific tweaks
+      font-thicken = true;
+      macos-titlebar-style = "hidden";
+      macos-option-as-alt = true;
+
+      background-opacity = 0.93;
+      # background-blur-radius = 50;
+      window-padding-x = 20;
+      window-padding-y = 20;
+    };
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
