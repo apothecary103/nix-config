3ff42f8d172a988c382fbc4dc363e0a77927b5e0
apothecary <113787039+yukariha@users.noreply.github.com>
2026-03-16 23:19:36 +0200

    feat: add zellij config and homebrew manager

--- flake.nix
+++ flake.nix
@@ -15,15 +15,30 @@
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs-darwin";
     };
+
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+
+    # Optional: Declarative tap management
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
   };
 
-  outputs = inputs @ { self, nixpkgs-darwin, darwin, home-manager, ... }: 
+  outputs = inputs @ { self, nixpkgs-darwin, darwin, home-manager, nix-homebrew, homebrew-core, homebrew-cask, ... }: 
   let
     username = "apothecary";
     system = "aarch64-darwin";
     hostname = "darwin";
 
-    specialArgs = inputs // { inherit username hostname; };
+    pkgs = nixpkgs-darwin.legacyPackages.${system};
+    orion-browser = pkgs.callPackage ./pkgs/orion-browser.nix { };
+
+    specialArgs = inputs // { inherit username hostname orion-browser; };
   in {
     darwinConfigurations."${hostname}" = darwin.lib.darwinSystem {
       inherit system specialArgs;
@@ -47,6 +62,35 @@
           # Optional: passes variables to home.nix
           home-manager.extraSpecialArgs = { inherit username; };
         }
+
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            # Install Homebrew under the default prefix
+            enable = true;
+
+            # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+            enableRosetta = false;
+
+            # User owning the Homebrew prefix
+            user = "${username}";
+
+            # Optional: Declarative tap management
+            taps = {
+              "homebrew/homebrew-core" = homebrew-core;
+              "homebrew/homebrew-cask" = homebrew-cask;
+            };
+
+            # Optional: Enable fully-declarative tap management
+            #
+            # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+            mutableTaps = false;
+          };
+        }
+        # Optional: Align homebrew taps config with nix-homebrew
+        ({config, ...}: {
+          homebrew.taps = builtins.attrNames config.nix-homebrew.taps;
+        })
       ];
     };
   };
