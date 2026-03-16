036d783fe431d0b244a4e1da98c52e121904bdce
apothecary <113787039+yukariha@users.noreply.github.com>
2026-03-16 23:36:28 +0200

    feat: add homebrew block and install helium-browser cask

--- modules/apps.nix
+++ modules/apps.nix
@@ -1,6 +1,26 @@
-{ pkgs, ...}: {
-
-environment.systemPackages = with pkgs; [
+{ pkgs, ... }:
+{
+  environment.systemPackages = with pkgs; [
     git
   ];
+
+  homebrew = {
+    enable = true;
+
+    # Updates homebrew packages on activation,
+    # can make darwin-rebuild slower but keeps things up to date.
+    onActivation.autoUpdate = true;
+
+    # 'zap' removes manually installed brews and casks that aren't in this file.
+    onActivation.cleanup = "zap";
+
+    casks = [
+      "helium-browser"
+    ];
+
+    # You can also install standard brew formulas here if they aren't in nixpkgs
+    brews = [
+      # "mas"
+    ];
+  };
 }
