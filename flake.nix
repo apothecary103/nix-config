055be17ae0805810605b26bc68d4a81b7d422028
apothecary <113787039+yukariha@users.noreply.github.com>
2026-03-21 09:54:16 +0200

    feat: restructure flake for darwin and nixos

--- flake.nix
+++ flake.nix
@@ -1,97 +1,32 @@
 {
-  description = "Nix for macOS configuration";
+  description = "My Nix Configuration";
 
   inputs = {
-    # nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-25.11-darwin";
-    nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     darwin = {
-      # url = "github:lnl7/nix-darwin/nix-darwin-25.11";
-      url = "github:lnl7/nix-darwin/master";
-      inputs.nixpkgs.follows = "nixpkgs-darwin";
+      url = "github:lnl7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-    # 1. Add Home Manager input (matching your version 25.11)
     home-manager = {
-      # url = "github:nix-community/home-manager/release-25.11";
-      url = "github:nix-community/home-manager/master";
-      inputs.nixpkgs.follows = "nixpkgs-darwin";
-    };
-
-    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
-
-    # Optional: Declarative tap management
-    homebrew-core = {
-      url = "github:homebrew/homebrew-core";
-      flake = false;
-    };
-    homebrew-cask = {
-      url = "github:homebrew/homebrew-cask";
-      flake = false;
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs @ { self, nixpkgs-darwin, darwin, home-manager, nix-homebrew, homebrew-core, homebrew-cask, ... }: 
+  outputs = inputs @ { self, nixpkgs, darwin, home-manager, ... }:
   let
     username = "apothecary";
-    system = "aarch64-darwin";
-    hostname = "darwin";
-
-    pkgs = nixpkgs-darwin.legacyPackages.${system};
-    orion-browser = pkgs.callPackage ./pkgs/orion-browser.nix { };
-
-    specialArgs = inputs // { inherit username hostname orion-browser; };
   in {
-    darwinConfigurations."${hostname}" = darwin.lib.darwinSystem {
-      inherit system specialArgs;
-      modules = [
-        ./modules/nix-core.nix
-        ./modules/system.nix
-        ./modules/apps.nix
-        ./modules/host-users.nix
+    darwinConfigurations."fern" = darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      specialArgs = { inherit inputs username; };
+      modules = [ ./modules/darwin ./home-entry.nix ];
+    };
 
-        # 2. Add the Home Manager darwin module
-        home-manager.darwinModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users."${username}" = {
-            imports = [
-              ./home.nix
-            ];
-          };
-          
-          # Optional: passes variables to home.nix
-          home-manager.extraSpecialArgs = { inherit username; };
-        }
-
-        nix-homebrew.darwinModules.nix-homebrew
-        {
-          nix-homebrew = {
-            # Install Homebrew under the default prefix
-            enable = true;
-
-            # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-            enableRosetta = false;
-
-            # User owning the Homebrew prefix
-            user = "${username}";
-
-            # Optional: Declarative tap management
-            taps = {
-              "homebrew/homebrew-core" = homebrew-core;
-              "homebrew/homebrew-cask" = homebrew-cask;
-            };
-
-            # Optional: Enable fully-declarative tap management
-            #
-            # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
-            mutableTaps = false;
-          };
-        }
-        # Optional: Align homebrew taps config with nix-homebrew
-        ({config, ...}: {
-          homebrew.taps = builtins.attrNames config.nix-homebrew.taps;
-        })
-      ];
+    nixosConfigurations."frieren" = nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";
+      specialArgs = { inherit inputs username; };
+      modules = [ ./modules/nixos ./home-entry.nix ];
     };
   };
 }
