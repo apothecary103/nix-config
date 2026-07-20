411c64b440f25aaebf8e5fb7e0b35201f73278d6
apothecary <frieren@noreply.codeberg.org>
2026-07-20 17:39:14 +0300

    feat: add zellij and nix-homebrew flake inputs

--- home.nix
+++ home.nix
@@ -161,6 +161,25 @@
     };
   };
 
+  programs.zellij = {
+    enable = true;
+    settings = {
+      pane_frames = false;
+      simplified_ui = false;
+      default_layout = "compact";
+      theme = "catppuccin-macchiato";
+
+      # copy_command = "pbcopy";
+      copy_on_select = true;
+      scroll_buffer_size = 10000;
+      ui = {
+        pane_frames = {
+          rounded_corners = false;
+        };
+      };
+    };
+  };
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
