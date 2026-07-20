7cf5ec0e67109a8963543db2de972f7d52a054c5
apothecary <frieren@noreply.codeberg.org>
2026-07-20 17:39:30 +0300

    refactor(frieren): move nixos host config into dendritic flake.modules.nixos

---
+++ modules/hosts/frieren.nix
@@ -0,0 +1,38 @@
+{
+  config,
+  inputs,
+  ...
+}: {
+  flake.modules.nixos."hosts/frieren" = {
+    imports = [
+      config.flake.modules.nixos.base
+      inputs.apple-silicon.nixosModules.apple-silicon-support
+    ];
+
+    networking.hostName = "frieren";
+    time.timeZone = "Europe/Vilnius";
+
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = false;
+    boot.kernelParams = [
+      "appledrm.show_notch=1"
+    ];
+    hardware.asahi.peripheralFirmwareDirectory = inputs.asahi-firmware;
+
+    nix.settings = {
+      cores = 0; # 0 = use all available cores
+      extra-substituters = [
+        "https://nixos-apple-silicon.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
+      ];
+    };
+
+    system.stateVersion = "25.11";
+  };
+
+  flake.nixosConfigurations.frieren = inputs.nixpkgs.lib.nixosSystem {
+    modules = [config.flake.modules.nixos."hosts/frieren"];
+  };
+}
