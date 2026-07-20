3d6997d3216b9be07f91bdf6e1dda3691179b0f9
apothecary <frieren@noreply.codeberg.org>
2026-07-20 17:39:20 +0300

    feat(fern): add daedra package

--- modules/darwin/packages.nix
+++ modules/darwin/packages.nix
@@ -1,19 +1,34 @@
 { pkgs, username, ... }:
 
+let
+  daedra = pkgs.rustPlatform.buildRustPackage rec {
+    pname = "daedra";
+    version = "0.3.2";
+    src = pkgs.fetchCrate {
+      inherit pname version;
+      hash = "sha256-Z5iDJKVZRtrlvS4BcP6c6khsGL8JL6+C1jmrRGkVlBk=";
+    };
+    cargoHash = "sha256-kbUgvAmgcSRgtIczn32xWcq75YUg4YGvUOyYUOdAKYQ=";
+    doCheck = false;
+    meta.mainProgram = "daedra";
+  };
+in
 {
   home-manager.users.${username} = {
-    home.packages = with pkgs; [
-      # CLI Tools
-      rustup
-    
-      # GUI Applications 
-      yabai
-      skhd
-      jankyborders
-      sketchybar
-      llama-cpp
-      opencode
-      # emacs-unstable
-    ];
+    home.packages =
+      (with pkgs; [
+        # CLI Tools
+        rustup
+
+        # GUI Applications
+        yabai
+        skhd
+        jankyborders
+        sketchybar
+        llama-cpp
+        opencode
+        # emacs-unstable
+      ])
+      ++ [ daedra ];
   };
 }
