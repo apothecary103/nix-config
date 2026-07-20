a65b998a6fe8d06101e327552a1180a5477dff8a
apothecary <frieren@noreply.codeberg.org>
2026-07-20 17:39:16 +0300

    feat(fern): add initial host config

---
+++ hosts/fern/default.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, inputs, username, ... }:
+
+{
+  imports = [
+    ../../modules/base
+    ../../modules/darwin
+    inputs.home-manager.nixosModules.home-manager
+  ];
+
+  networking.hostName = "fern";
+
+  users.users.${username} = {
+    name = username;
+    home = "/Users/${username}";
+    description = username;
+  };
+  nix.settings.trusted-users = [ username ];
+
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    useExtraSpecialArgs = { inherit inputs username; hostname = "fern"; };
+    users.${username} = import ../../home/default.nix;
+  };
+
+  system.stateVersion = "5";
+}
