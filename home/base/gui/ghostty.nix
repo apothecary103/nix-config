108181e69c60d69466d1e5563f27f3d1076e3632
apothecary <frieren@noreply.codeberg.org>
2026-07-20 17:39:16 +0300

    chore: minor ghostty/yazi tweaks and gitignore update

--- home/base/gui/ghostty.nix
+++ home/base/gui/ghostty.nix
@@ -1,21 +1,25 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.ghostty = {
     enable = true;
-    package = pkgs.ghostty-bin;
+    package = if pkgs.stdenv.isDarwin then pkgs.ghostty-bin else pkgs.ghostty;
     settings = {
       theme = "Catppuccin Macchiato";
       font-family = "Maple Mono NF CN";
-      font-size = 18;
+      font-size = if pkgs.stdenv.isDarwin then 18 else 12;
 
       # macOS specific tweaks
       font-thicken = true;
       macos-titlebar-style = "hidden";
       macos-option-as-alt = true;
 
+      # Wayland specific tweaks
+      window-decoration = "server";
+
+      # background-opacity = if pkgs.stdenv.isDarwin then 0.93 else 0.98;
       background-opacity = 0.93;
       # background-blur-radius = 50;
       window-padding-x = 20;
-      window-padding-y = 20;
+      window-padding-y = 10;
     };
   };
 }
