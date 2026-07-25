efe89050425ef903bb81ea3109a4f7473208eb2e
apothecary <frieren@noreply.codeberg.org>
2026-07-25 20:41:27 +0300

    refactor(term): drop tmux keybinds from ghostty

--- modules/programs/term/ghostty.nix
+++ modules/programs/term/ghostty.nix
@@ -22,31 +22,6 @@
         background-opacity = 0.93;
         window-padding-x = 20;
         window-padding-y = 10;
-
-        # tmux (prefix = Ctrl+a)
-        keybind = [
-          # Switch tmux windows
-          "cmd+digit_1=text:\\x011"
-          "cmd+digit_2=text:\\x012"
-          "cmd+digit_3=text:\\x013"
-          "cmd+digit_4=text:\\x014"
-          "cmd+digit_5=text:\\x015"
-          "cmd+digit_6=text:\\x016"
-          "cmd+digit_7=text:\\x017"
-          "cmd+digit_8=text:\\x018"
-          "cmd+digit_9=text:\\x019"
-
-          # tmux window management
-          "cmd+t=text:\\x01c"
-          "cmd+w=text:\\x01&"
-
-          # Navigate tmux windows
-          "cmd+left=text:\\x01p"
-          "cmd+right=text:\\x01n"
-
-          # Last tmux window
-          "cmd+tab=text:\\x01l"
-        ];
       };
     };
   };
