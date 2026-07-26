f7859dbb4c3cbdc0825bd786a2d15565e0905614
apothecary <frieren@noreply.codeberg.org>
2026-07-26 15:11:56 +0300

    feat(programs): opt out of common CLI telemetry

---
+++ modules/programs/telemetry.nix
@@ -0,0 +1,9 @@
+{
+  flake.modules.homeManager.base.home.sessionVariables = {
+    DO_NOT_TRACK = "1";
+    DOTNET_CLI_TELEMETRY_OPTOUT = "1";
+    NEXT_TELEMETRY_DISABLED = "1";
+    GATSBY_TELEMETRY_DISABLED = "1";
+    HF_HUB_DISABLE_TELEMETRY = "1";
+  };
+}
