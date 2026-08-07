06f0525a1e55c288af412a85fce99f90004c31a8
apothecary <frieren@noreply.codeberg.org>
2026-08-07 04:08:47 +0300

    style(nix-index): list the nixos block before the darwin one

--- modules/system/nix-index.nix
+++ modules/system/nix-index.nix
@@ -1,11 +1,12 @@
-{ inputs, ... }: {
-  flake.modules.darwin.base = {
-    imports = [ inputs.nix-index-database.darwinModules.default ];
+{ inputs, ... }:
+{
+  flake.modules.nixos.base = {
+    imports = [ inputs.nix-index-database.nixosModules.default ];
     programs.nix-index-database.comma.enable = true;
   };
 
-  flake.modules.nixos.base = {
-    imports = [ inputs.nix-index-database.nixosModules.default ];
+  flake.modules.darwin.base = {
+    imports = [ inputs.nix-index-database.darwinModules.default ];
     programs.nix-index-database.comma.enable = true;
   };
 }
