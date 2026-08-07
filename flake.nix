eaaeae5166a89923ce83d1d2be544e5e69db8e1e
apothecary <frieren@noreply.codeberg.org>
2026-08-07 04:05:37 +0300

    chore(helix): track upstream instead of the steel fork

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     preservation.url = "github:nix-community/preservation";
-    helix.url = "github:mattwparas/helix/steel-event-system";
+    helix.url = "github:helix-editor/helix";
     niri = {
       url = "github:epireyn/niri-flake";
       inputs.nixpkgs.follows = "nixpkgs";
