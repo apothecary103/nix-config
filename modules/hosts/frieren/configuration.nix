9aa7d95ee00b4bce9825d7286f473eb4ac898987
apothecary <frieren@noreply.codeberg.org>
2026-08-28 19:57:58 +0300

    style(frieren): group the boot options

--- modules/hosts/frieren/configuration.nix
+++ modules/hosts/frieren/configuration.nix
@@ -13,14 +13,18 @@
     networking.hostName = "frieren";
     time.timeZone = "Europe/Vilnius";
 
-    boot.loader.limine.enable = true;
-    boot.loader.efi.canTouchEfiVariables = false;
-    boot.kernelParams = [
-      "appledrm.show_notch=1"
-    ];
+    boot = {
+      loader = {
+        limine.enable = true;
+        efi.canTouchEfiVariables = false;
+      };
+      kernelParams = [ "appledrm.show_notch=1" ];
+    };
     hardware.asahi.peripheralFirmwareDirectory = inputs.asahi-firmware;
     hardware.asahi.enable = true;
 
+    # This records the on-disk schema from the original installation. Do not
+    # bump it when updating nixpkgs; only change it for a deliberate reinstall.
     system.stateVersion = "25.11";
   };
 
